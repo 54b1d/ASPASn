27c1b8399c421ba2c870696603959725dd35a81e
54b1d <sabidul1@gmail.com>
2022-05-02 15:31:20 +0600

    CreateAccount: Initial Commit Moved form to Create account activity

---
+++ app/src/main/java/com/sabid/aspasn/CreateAccountActivity.java
@@ -0,0 +1,78 @@
+package com.sabid.aspasn;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class CreateAccountActivity extends AppCompatActivity {
+    EditText name, address, mobile;
+    Button confirmAddAccount, viewAccounts;
+    DBHelper DB;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View _v) {
+                    onBackPressed();
+                }});
+
+        name = findViewById(R.id.editName);
+        address = findViewById(R.id.editAddress);
+        mobile = findViewById(R.id.editMobile);
+
+        confirmAddAccount = findViewById(R.id.btnConfirmAddAccount);
+        viewAccounts = findViewById(R.id.btnConfirmAddAccount);
+        
+        DB = new DBHelper(this);
+
+        confirmAddAccount.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    String nameTXT = name.getText().toString();
+                    String addressTXT = address.getText().toString();
+                    String mobileTXT = mobile.getText().toString();
+
+                    Boolean checkinsertdata = DB.insertuserdata(nameTXT, addressTXT, mobileTXT);
+                    if (checkinsertdata) {
+                        Toast.makeText(CreateAccountActivity.this, "New Entry Inserted", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(CreateAccountActivity.this, "Not Inserted", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        viewAccounts.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Cursor res = DB.getdata();
+                    if (res.getCount() == 0) {
+                        Toast.makeText(CreateAccountActivity.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    StringBuffer buffer = new StringBuffer();
+                    while (res.moveToNext()) {
+                        buffer.append("Name: " + res.getString(0) + "\n");
+                        buffer.append("Contact: " + res.getString(1) + "\n");
+                        buffer.append("Date of Birth: " + res.getString(2) + "\n\n");
+                    }
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CreateAccountActivity.this);
+                    builder.setCancelable(true);
+                    builder.setTitle("user Entries");
+                    builder.setMessage(buffer.toString());
+                    builder.show();
+                }
+            });
+
+    }
+
+}
--- app/src/main/java/com/sabid/aspasn/MainActivity.java
+++ app/src/main/java/com/sabid/aspasn/MainActivity.java
@@ -1,123 +1,55 @@
 package com.sabid.aspasn;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.widget.EditText;
-import android.widget.Button;
-import android.widget.Toast;
-import android.database.Cursor;
-import android.app.AlertDialog;
-import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        final EditText name, contact,  dob;
-        Button insert, update, delete, view, btnActivityCalc;
-        final DBHelper DB;
+        Button btnActivityCalc, btnActivityCreateAccount;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getSupportActionBar().setHomeButtonEnabled(true);
+        /**toolbar.setNavigationOnClickListener(new View.OnClickListener() {
                 @Override public void onClick(View _v) {
                     onBackPressed();
-                }});
+                }});*/
 
-        name = findViewById(R.id.edit_name);
-        contact = findViewById(R.id.edit_contact);
-        dob = findViewById(R.id.edit_dob);
 
-        insert = findViewById(R.id.btn_insert);
-        update = findViewById(R.id.btn_update);
-        delete = findViewById(R.id.btn_delete);
-        view = findViewById(R.id.btn_view);
         btnActivityCalc = findViewById(R.id.btnActivityCalc);
-        DB = new DBHelper(this);
+        btnActivityCreateAccount = findViewById(R.id.btnActivityCreateAccount);
+        
         
         btnActivityCalc.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                openCalculatorActivity();
-            }
-        });
-
-        insert.setOnClickListener(new View.OnClickListener(){
                 @Override
-                public void onClick(View v) {
-                    String nameTXT = name.getText().toString();
-                    String contactTXT = contact.getText().toString();
-                    String dobTXT = dob.getText().toString();
-
-                    Boolean checkinsertdata = DB.insertuserdata(nameTXT, contactTXT, dobTXT);
-                    if (checkinsertdata) {
-                        Toast.makeText(MainActivity.this, "New Entry Inserted", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Not Inserted", Toast.LENGTH_SHORT).show();
-                    }
+                public void onClick(View v){
+                    openCalculatorActivity();
                 }
             });
-        update.setOnClickListener(new View.OnClickListener(){
+        btnActivityCreateAccount.setOnClickListener(new View.OnClickListener(){
                 @Override
-                public void onClick(View v) {
-                    String nameTXT = name.getText().toString();
-                    String contactTXT = contact.getText().toString();
-                    String dobTXT = dob.getText().toString();
-
-                    Boolean checkupdatedata = DB.updateuserdata(nameTXT, contactTXT, dobTXT);
-                    if (checkupdatedata) {
-                        Toast.makeText(MainActivity.this, "Entry Updated", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Not Updated", Toast.LENGTH_SHORT).show();
-                    }
+                public void onClick(View v){
+                    openCreateAccountActivity();
                 }
             });
 
-        delete.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    String nameTXT = name.getText().toString();
-
-
-                    Boolean checkdeletedata = DB.deletedata(nameTXT);
-                    if (checkdeletedata) {
-                        Toast.makeText(MainActivity.this, "Entry Deleted", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Not Deleted", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-
-        view.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v) {
-                    Cursor res = DB.getdata();
-                    if (res.getCount() == 0) {
-                        Toast.makeText(MainActivity.this, "No Entry Exists", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                    StringBuffer buffer = new StringBuffer();
-                    while (res.moveToNext()) {
-                        buffer.append("Name: " + res.getString(0) + "\n");
-                        buffer.append("Contact: " + res.getString(1) + "\n");
-                        buffer.append("Date of Birth: " + res.getString(2) + "\n\n");
-                    }
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setCancelable(true);
-                    builder.setTitle("user Entries");
-                    builder.setMessage(buffer.toString());
-                    builder.show();
-                }
-            });
+        
 
     }
     public void openCalculatorActivity(){
         Intent intent = new Intent(this, CalculatorActivity.class);
+        startActivity(intent);
+    }
+    public void openCreateAccountActivity(){
+        Intent intent = new Intent(this, CreateAccountActivity.class);
         startActivity(intent);
     }
 
