1f8414999d8835704cec0d84f5f3bd431a4dfc75
54b1d <sabidul1@gmail.com>
2022-05-09 00:05:47 +0600

    DatePicker Test

--- app/src/main/java/com/sabid/aspasn/AddCashTransactionActivity.java
+++ app/src/main/java/com/sabid/aspasn/AddCashTransactionActivity.java
@@ -1,17 +1,24 @@
 package com.sabid.aspasn;
 
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.database.Cursor;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
 import java.util.ArrayList;
 
 public class AddCashTransactionActivity extends AppCompatActivity {
     AutoCompleteTextView editAutoName;
+    EditText editDate;
     ArrayList accounts;
     DBHelper DB;
     @Override
@@ -24,15 +31,39 @@ public class AddCashTransactionActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View _v) {
-                    onBackPressed();
-                }
-            });
+            @Override
+            public void onClick(View _v) {
+                onBackPressed();
+            }
+        });
 
         editAutoName = findViewById(R.id.editAutoName);
+        editDate = findViewById(R.id.editDate);
+
         DB = new DBHelper(this);
         loadEditAutoName();
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        editDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getApplicationContext(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int day) {
+                        month = month + 1;
+                        String date = day + "/" + month + "/" + year;
+                        editDate.setText(date);
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
+
+
     }
 
     public void loadEditAutoName() {
