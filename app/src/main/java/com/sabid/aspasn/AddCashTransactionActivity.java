a450e8d1bd8df766c555d1bdc3d5733c2478ec5c
54b1d <sabidul1@gmail.com>
2022-05-10 23:08:28 +0600

    AddCashTransaction: Fixed if else logic

--- app/src/main/java/com/sabid/aspasn/AddCashTransactionActivity.java
+++ app/src/main/java/com/sabid/aspasn/AddCashTransactionActivity.java
@@ -18,108 +18,107 @@ import androidx.appcompat.widget.Toolbar;
 import java.util.ArrayList;
 
 public class AddCashTransactionActivity extends AppCompatActivity {
-    String date, name, amount;
-    AutoCompleteTextView editAutoName;
-    EditText editDate, editAmount;
-    Button btnConfirmCashReceiptTransaction, btnConfirmCashPaymentTransaction;
-    ArrayList accounts;
-    Boolean isReceipt, isPayment;
-    DBHelper DB;
+	String date, name, amount;
+	AutoCompleteTextView editAutoName;
+	EditText editDate, editAmount;
+	Button btnConfirmCashReceiptTransaction, btnConfirmCashPaymentTransaction;
+	ArrayList accounts;
+	Boolean isReceipt, isPayment;
+	DBHelper DB;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_cash_transaction);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("New Cash Transaction");
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View _v) {
-                onBackPressed();
-            }
-        });
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_cash_transaction);
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		toolbar.setTitle("New Cash Transaction");
+		setSupportActionBar(toolbar);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getSupportActionBar().setHomeButtonEnabled(true);
+		toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View _v) {
+				onBackPressed();
+			}
+		});
 
-        editAutoName = findViewById(R.id.editAutoName);
-        editDate = findViewById(R.id.editDate);
-        editAmount = findViewById(R.id.editAmount);
-        btnConfirmCashReceiptTransaction = findViewById(R.id.btnConfirmCashReceiptTransaction);
-        btnConfirmCashPaymentTransaction = findViewById(R.id.btnConfirmPaymentTransaction);
+		editAutoName = findViewById(R.id.editAutoName);
+		editDate = findViewById(R.id.editDate);
+		editAmount = findViewById(R.id.editAmount);
+		btnConfirmCashReceiptTransaction = findViewById(R.id.btnConfirmCashReceiptTransaction);
+		btnConfirmCashPaymentTransaction = findViewById(R.id.btnConfirmPaymentTransaction);
 
-        DB = new DBHelper(this);
-        loadEditAutoName();
+		DB = new DBHelper(this);
+		loadEditAutoName();
 
-        Calendar calendar = Calendar.getInstance();
-        final int year = calendar.get(Calendar.YEAR);
-        final int month = calendar.get(Calendar.MONTH);
-        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+		Calendar calendar = Calendar.getInstance();
+		final int year = calendar.get(Calendar.YEAR);
+		final int month = calendar.get(Calendar.MONTH);
+		final int day = calendar.get(Calendar.DAY_OF_MONTH);
 
-        editDate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                DatePickerDialog datePickerDialog = new DatePickerDialog(AddCashTransactionActivity.this, R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int day) {
-                        month = month + 1;
-                        String date = day + "/" + month + "/" + year;
-                        editDate.setText(date);
-                    }
-                }, year, month, day);
+		editDate.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				DatePickerDialog datePickerDialog = new DatePickerDialog(AddCashTransactionActivity.this,
+						R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
+							@Override
+							public void onDateSet(DatePicker view, int year, int month, int day) {
+								month = month + 1;
+								String date = day + "/" + month + "/" + year;
+								editDate.setText(date);
+							}
+						}, year, month, day);
 
-                datePickerDialog.show();
-            }
-        });
+				datePickerDialog.show();
+			}
+		});
 
-        btnConfirmCashReceiptTransaction.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                date = editDate.getText().toString();
-                name = editAutoName.getText().toString();
-                amount = editAmount.getText().toString();
-                cashTransactionConfirmed(isReceipt, date, name, amount);
-            }
-        });
+		btnConfirmCashReceiptTransaction.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				cashTransactionConfirmed(isReceipt = true);
+			}
+		});
 
-        btnConfirmCashPaymentTransaction.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                date = editDate.getText().toString();
-                name = editAutoName.getText().toString();
-                amount = editAmount.getText().toString();
-                cashTransactionConfirmed(isPayment, date, name, amount);
-            }
-        });
+		btnConfirmCashPaymentTransaction.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				cashTransactionConfirmed(isReceipt = false);
+			}
+		});
 
+	}
 
-    }
+	public void loadEditAutoName() {
+		accounts = new ArrayList<String>();
+		Cursor res = DB.getAccounts();
+		if (res.getCount() == 0) {
+			accounts.add("No Available Account");
+		} else {
+			while (res.moveToNext()) {
+				//take category name only from 2nd(1) column
+				accounts.add(res.getString(1));
 
-    public void loadEditAutoName() {
-        accounts = new ArrayList<String>();
-        Cursor res = DB.getAccounts();
-        if (res.getCount() == 0) {
-            accounts.add("No Available Account");
-        } else {
-            while (res.moveToNext()) {
-                //take category name only from 2nd(1) column
-                accounts.add(res.getString(1));
+			}
+		}
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.select_dialog_item, accounts);
+		editAutoName.setThreshold(1);
+		editAutoName.setAdapter(adapter);
+		res.close();
+	}
 
-            }
-        }
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>
-                (this, android.R.layout.select_dialog_item, accounts);
-        editAutoName.setThreshold(1);
-        editAutoName.setAdapter(adapter);
-        res.close();
-    }
+	public void cashTransactionConfirmed(Boolean isReceipt) {
+		date = editDate.getText().toString();
+		name = editAutoName.getText().toString();
+		amount = editAmount.getText().toString();
 
-    public void cashTransactionConfirmed(Boolean isReceipt, String date, String name, String amount) {
-        if (!isReceipt) {
-            Toast.makeText(this, "Paid " + amount + "Tk To " + name, Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Received " + amount + "Tk From " + name, Toast.LENGTH_SHORT).show();
-        }
-    }
+		if (isReceipt) {
+			//todo call dbhelper function
+			Toast.makeText(this, "Received " + amount + "Tk From " + name, Toast.LENGTH_SHORT).show();
+
+		} else {
+			Toast.makeText(this, "Paid " + amount + "Tk To " + name, Toast.LENGTH_SHORT).show();
+		}
+	}
 
 }
