a833cac82222b69ba4e6aef10bd8eca6cbfab42c
54b1d <sabidul1@gmail.com>
2022-06-14 18:51:31 +0600

    Navigation Drawer: Placeholder Implementation
    Might breaks other activities due to res/styles.xml  changes

--- app/src/main/java/com/sabid/aspasn/MainActivity.java
+++ app/src/main/java/com/sabid/aspasn/MainActivity.java
@@ -1,40 +1,77 @@
 package com.sabid.aspasn;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 
 public class MainActivity extends AppCompatActivity {
-	DBHelper DB;
+
+
+    public DrawerLayout drawerLayout;
+
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
-        Button btnActivityDebugButtons;
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        Toolbar toolbar=findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //getSupportActionBar().setHomeButtonEnabled(true);
-        /**toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-                @Override public void onClick(View _v) {
-                    onBackPressed();
-                }});*/
-		DB = new DBHelper(this);
-        btnActivityDebugButtons = findViewById(R.id.btnActivityDebugButtons);
-        btnActivityDebugButtons.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View v){
-                    openDebugButtonsActivity();
-                }
-            });
+
+
+        // drawer layout instance to toggle the menu icon to open 
+
+        // drawer and back button to close drawer
+
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+
+        // pass the Open and Close toggle for the drawer layout listener
+
+        // to toggle the button
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        actionBarDrawerToggle.syncState();
+
+
+        // to make the Navigation drawer icon always appear on the action bar
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
-    
-    public void openDebugButtonsActivity(){
-        Intent intent = new Intent(this, DebugButtonsActivity.class);
-        startActivity(intent);
+
+
+    // override the onOptionsItemSelected()
+
+    // function to implement 
+
+    // the item click listener callback
+
+    // to open and close the navigation 
+
+    // drawer when the icon is clicked
+
+    @Override
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 }
